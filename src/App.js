f2c2db889b09b1ac231489b3b0e162064a275c32
Lindsay Zhang <lindsay.zhang@thescore.com>
2024-10-04 12:06:23 -0400

    basic window component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import './components/header';
 import Header from './components/header';
 import background from './resources/windows.png'
+import Clickable from './components/clickable'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='bg'>
       {/* <img class="bg" src={background} alt="" /> */}
+      {/* <Home></Home> */}
+      <Clickable title={"ILOVEYOU"} text={"hi"} />
       <Header></Header>
       {/* <emailPopup></emailPopup> */}
     </div>
