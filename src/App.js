304307d482f2b76c36a666f1cbdcea1f7997c65b
Lindsay <zhlindsay04@gmail.com>
2024-11-23 00:14:27 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import './components/header';
-import Header from './components/header';
-import background from './resources/windows.png'
-import Clickable from './components/clickable'
-import Y2kTimer from './components/y2kTimer'
-import Home from './pages/Home';
-import Window from './components/window';
+import './background.css'
 
 function App() {
   return (
-    <div className='bg'>
-      {/* <img class="bg" src={background} alt="" /> */}
-      {/* <Home></Home> */}
-      <Clickable title={"ILOVEYOU"} text={"hi"} />
-      <Y2kTimer />
-      <Header></Header>
-      <Window></Window>
-      {/* <emailPopup></emailPopup> */}
+    <div class = "background" style={{ minHeight: '100vh' }}>
+      
     </div>
   );
 }
