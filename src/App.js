581a13c5bb54f5280aa10124e02cd5bac592c6a9
Lindsay <zhlindsay04@gmail.com>
2025-01-12 16:15:24 -0500

    made with love

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ItsMeFrog from './pages/itsMeFrog.js';
+import footer from './resources/misc/footer.png'
 
 function App() {
   return (
     <div>
       <ItsMeFrog />
+      <img class = 'footer' src = {footer}/>
     </div>
   );
 }
